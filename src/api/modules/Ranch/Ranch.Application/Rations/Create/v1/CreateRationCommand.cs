49edaaa0da329656dfdd29f23d064ade71fb4f4f
Eric Owens <eric.owens@capstone-s.com>
2025-05-02 07:51:20 -0600

    update CreateRationCommand to not use default value annotation

--- src/api/modules/Ranch/Ranch.Application/Rations/Create/v1/CreateRationCommand.cs
+++ src/api/modules/Ranch/Ranch.Application/Rations/Create/v1/CreateRationCommand.cs
@@ -2,9 +2,10 @@ using System.ComponentModel;
 using MediatR;
 
 namespace FSH.Starter.WebApi.Ranch.Application.Rations.Create.v1;
+
 public sealed record CreateRationCommand(
-    [property: DefaultValue("Sample Ration")] string? Name,
-    [property: DefaultValue("Ration Description")] string? Description,
-    [property: DefaultValue(0.10)] decimal DollarsPerPound
+    string? Name,
+    string? Description,
+    decimal DollarsPerPound
 ) : IRequest<CreateRationResponse>;
 

0e442ea8380d0bde84cd8ae7b4fa323fb4fe960d
Eric Owens <eric.owens@capstone-s.com>
2025-05-02 10:45:04 -0600

    restore the property annotations so rewriter works again

--- src/api/modules/Ranch/Ranch.Application/Rations/Create/v1/CreateRationCommand.cs
+++ src/api/modules/Ranch/Ranch.Application/Rations/Create/v1/CreateRationCommand.cs
@@ -4,8 +4,8 @@ using MediatR;
 namespace FSH.Starter.WebApi.Ranch.Application.Rations.Create.v1;
 
 public sealed record CreateRationCommand(
-    string? Name,
-    string? Description,
-    decimal DollarsPerPound
+    [property: DefaultValue("Sample Ration")] string? Name,
+    [property: DefaultValue("Descriptive Description")] string? Description,
+    [property: DefaultValue(0.1)] decimal DollarsPerPound
 ) : IRequest<CreateRationResponse>;
 

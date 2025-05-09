b9fbfce44f24fab8a862fff2834a7d225d3652d4
Eric Owens <eric.owens@capstone-s.com>
2025-05-09 09:08:21 -0600

    add TODO Member comment

--- src/api/modules/Members/Members.Application/MemberAds/Create/v1/CreateMemberAdHandler.cs
+++ src/api/modules/Members/Members.Application/MemberAds/Create/v1/CreateMemberAdHandler.cs
@@ -17,6 +17,7 @@ public sealed class CreateMemberAdHandler(
     public async Task<CreateMemberAdResponse> Handle(CreateMemberAdCommand request, CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(request);
+        //TODO Member Start
         var currentTenantId = multiTenantContextAccessor.MultiTenantContext?.TenantInfo?.Id ?? throw new InvalidOperationException("TenantId is not set.");
         var searchCommand = new SearchMemberAdsCommand();
         searchCommand.TenantId = currentTenantId;
@@ -26,7 +27,7 @@ public sealed class CreateMemberAdHandler(
         {
             throw new InvalidOperationException("Tenant already has a member ad.");
         }
-
+        //TODO Member End
         var memberAd = MemberAd.Create(
             request.TenantId,
             request.Name,

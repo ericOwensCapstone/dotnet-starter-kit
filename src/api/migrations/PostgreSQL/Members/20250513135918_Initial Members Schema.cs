b0dcd72cdeb4d271e7c9bf3d028a3406b343ed14
Eric Owens <eric.owens@capstone-s.com>
2025-05-13 08:16:30 -0600

    validating new MemberId property on entities

---
+++ src/api/migrations/PostgreSQL/Members/20250513135918_Initial Members Schema.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSH.Starter.WebApi.Migrations.PostgreSQL.Members
+{
+    /// <inheritdoc />
+    public partial class InitialMembersSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "members");
+
+            migrationBuilder.CreateTable(
+                name: "MemberAds",
+                schema: "members",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TenantId = table.Column<string>(type: "text", nullable: true),
+                    MemberId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Name = table.Column<string>(type: "character varying(99)", maxLength: 99, nullable: false),
+                    Description = table.Column<string>(type: "character varying(999)", maxLength: 999, nullable: true),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    Deleted = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MemberAds", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MemberAds_Name_TenantId",
+                schema: "members",
+                table: "MemberAds",
+                columns: new[] { "Name", "TenantId" },
+                unique: true,
+                filter: "\"Deleted\" IS NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MemberAds",
+                schema: "members");
+        }
+    }
+}

97f5d1fc0c5b17a425bee7828d473e85a58813ec
Eric Owens <eric.owens@capstone-s.com>
2025-05-15 10:27:38 -0600

    refactor for rewriter

--- src/api/modules/Ranch/Ranch.Infrastructure/RanchModule.cs
+++ src/api/modules/Ranch/Ranch.Infrastructure/RanchModule.cs
@@ -8,10 +8,14 @@ using Microsoft.Extensions.DependencyInjection;
 using FSH.Starter.WebApi.Ranch.Infrastructure.Persistence;
 using FSH.Starter.WebApi.Ranch.Domain.Rations;
 using FSH.Starter.WebApi.Ranch.Infrastructure.Endpoints.v1.Rations;
-// Start ContractStatus
+// Start ContractStatus;
 using FSH.Starter.WebApi.Ranch.Domain.ContractStatuses;
 using FSH.Starter.WebApi.Ranch.Infrastructure.Endpoints.v1.ContractStatuses;
-// End ContractStatus
+// End ContractStatus;
+// Start Contract;
+using FSH.Starter.WebApi.Ranch.Domain.Contracts;
+using FSH.Starter.WebApi.Ranch.Infrastructure.Endpoints.v1.Contracts;
+// End Contract;
 namespace FSH.Starter.WebApi.Ranch.Infrastructure;
 public static class RanchModule
 {
@@ -26,14 +30,46 @@ public static class RanchModule
             rationGroup.MapGetRationListEndpoint();
             rationGroup.MapRationUpdateEndpoint();
             rationGroup.MapRationDeleteEndpoint();       
-            // Start ContractStatus
+            // Start ContractStatus;
             var contractStatusGroup = app.MapGroup("contractStatuses").WithTags("contractStatuses");
             contractStatusGroup.MapContractStatusCreationEndpoint();
             contractStatusGroup.MapGetContractStatusEndpoint();
             contractStatusGroup.MapGetContractStatusListEndpoint();
             contractStatusGroup.MapContractStatusUpdateEndpoint();
             contractStatusGroup.MapContractStatusDeleteEndpoint();
-            // End ContractStatus
+            // End ContractStatus;
+            // Start Contract;
+            var contractGroup = app.MapGroup("contracts").WithTags("contracts");
+            contractGroup.MapContractCreationEndpoint();
+            contractGroup.MapGetContractEndpoint();
+            contractGroup.MapGetContractListEndpoint();
+            contractGroup.MapContractUpdateEndpoint();
+            contractGroup.MapContractDeleteEndpoint();
+            // End Contract;
+            // Start Contract;
+            var contractGroup = app.MapGroup("contracts").WithTags("contracts");
+            contractGroup.MapContractCreationEndpoint();
+            contractGroup.MapGetContractEndpoint();
+            contractGroup.MapGetContractListEndpoint();
+            contractGroup.MapContractUpdateEndpoint();
+            contractGroup.MapContractDeleteEndpoint();
+            // End Contract;
+            // Start Contract;
+            var contractGroup = app.MapGroup("contracts").WithTags("contracts");
+            contractGroup.MapContractCreationEndpoint();
+            contractGroup.MapGetContractEndpoint();
+            contractGroup.MapGetContractListEndpoint();
+            contractGroup.MapContractUpdateEndpoint();
+            contractGroup.MapContractDeleteEndpoint();
+            // End Contract;
+            // Start Contract;
+            var contractGroup = app.MapGroup("contracts").WithTags("contracts");
+            contractGroup.MapContractCreationEndpoint();
+            contractGroup.MapGetContractEndpoint();
+            contractGroup.MapGetContractListEndpoint();
+            contractGroup.MapContractUpdateEndpoint();
+            contractGroup.MapContractDeleteEndpoint();
+            // End Contract;
         }
     }
     public static WebApplicationBuilder RegisterRanchServices(this WebApplicationBuilder builder)
@@ -43,10 +79,26 @@ public static class RanchModule
         builder.Services.AddScoped<IDbInitializer, RanchDbInitializer>();
         builder.Services.AddKeyedScoped<IRepository<Ration>, RanchRepository<Ration>>("ranch:rations");
         builder.Services.AddKeyedScoped<IReadRepository<Ration>, RanchRepository<Ration>>("ranch:rations");
-        // Start ContractStatus
+        // Start ContractStatus;
         builder.Services.AddKeyedScoped<IRepository<ContractStatus>, RanchRepository<ContractStatus>>("ranch:contractStatuses");
         builder.Services.AddKeyedScoped<IReadRepository<ContractStatus>, RanchRepository<ContractStatus>>("ranch:contractStatuses");
-        // End ContractStatus
+        // End ContractStatus;
+        // Start Contract;
+        builder.Services.AddKeyedScoped<IRepository<Contract>, RanchRepository<Contract>>("ranch:contracts");
+        builder.Services.AddKeyedScoped<IReadRepository<Contract>, RanchRepository<Contract>>("ranch:contracts");
+        // End Contract;
+        // Start Contract;
+        builder.Services.AddKeyedScoped<IRepository<Contract>, RanchRepository<Contract>>("ranch:contracts");
+        builder.Services.AddKeyedScoped<IReadRepository<Contract>, RanchRepository<Contract>>("ranch:contracts");
+        // End Contract;
+        // Start Contract;
+        builder.Services.AddKeyedScoped<IRepository<Contract>, RanchRepository<Contract>>("ranch:contracts");
+        builder.Services.AddKeyedScoped<IReadRepository<Contract>, RanchRepository<Contract>>("ranch:contracts");
+        // End Contract;
+        // Start Contract;
+        builder.Services.AddKeyedScoped<IRepository<Contract>, RanchRepository<Contract>>("ranch:contracts");
+        builder.Services.AddKeyedScoped<IReadRepository<Contract>, RanchRepository<Contract>>("ranch:contracts");
+        // End Contract;
         return builder;
     }
     public static WebApplication UseRanchModule(this WebApplication app)

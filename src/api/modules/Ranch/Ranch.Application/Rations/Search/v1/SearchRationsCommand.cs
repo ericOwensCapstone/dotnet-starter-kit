a456b3717c1b506f5316570bbaab1c15e0c0a44c
Eric Owens <eric.owens@capstone-s.com>
2025-05-15 12:30:39 -0600

    add tags in search command for custom fields

--- src/api/modules/Ranch/Ranch.Application/Rations/Search/v1/SearchRationsCommand.cs
+++ src/api/modules/Ranch/Ranch.Application/Rations/Search/v1/SearchRationsCommand.cs
@@ -6,6 +6,8 @@ namespace FSH.Starter.WebApi.Ranch.Application.Rations.Search.v1;
 
 public class SearchRationsCommand : PaginationFilter, IRequest<PagedList<RationResponse>>
 {
+    //Start Custom Fields
+    //End Custom Fields
     //Start One Per Code
     //End One Per Code
 }
